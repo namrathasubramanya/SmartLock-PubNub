a28658077e6cc13abb4fd94ec960bc27aa38bb51
namrathasubramanya <namratha.subramanya@gmail.com>
2018-05-30 13:10:37 -0700

    Remove keys

--- validKey.js
+++ validKey.js
@@ -1,8 +1,8 @@
 function unlock() {
 	var pubnub = new PubNub({
-	subscribeKey: 'sub-c-a1ad7926-5e0a-11e8-8ebf-f686a6d93a6b',
-    publishKey: 'pub-c-563cbabc-a399-4a76-830a-468a698efd4d',
-    authKey: 'Valid_key'
+	subscribeKey: 'Enter your subscribe key here',
+    	publishKey: 'Enter your publish key here',
+    	authKey: 'Valid_key'
 });
 
 pubnub.publish({
